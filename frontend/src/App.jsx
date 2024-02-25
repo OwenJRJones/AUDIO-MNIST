ec6516e4f50e7fbffc8fcb93ddf9cbfad71e06d1
Owen Jones <71284174+OwenJRJones@users.noreply.github.com>
2024-02-25 15:46:52 -0400

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,7 +8,13 @@ function App() {
 	return (
 		<QueryClientProvider client={queryClient}>
 			<h1>Audio MNIST</h1>
+			<p className="description">
+				Say a digit from 0-9 and the network will return a prediction.
+			</p>
 			<AudioRecorder />
+			<p className="foot-note">
+				*Note: Please allow a few seconds for the first prediction to be produced.
+			</p>
 		</QueryClientProvider>
 	);
 }
